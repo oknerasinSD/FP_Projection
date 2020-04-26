b60a8677415eb896b7233769ad533139f7be197e
oknerasinSD <d.oknerasins@gmail.com>
2020-04-26 13:04:19 +0300

    Implement more Bootstrap on login, register and main pages

--- src/main/java/com/example/mvc1/domain/User.java
+++ src/main/java/com/example/mvc1/domain/User.java
@@ -16,11 +16,14 @@ public class User implements UserDetails {
     private String username;
     private String password;
     private boolean active;
-
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
 
     public Long getId() {
         return id;
