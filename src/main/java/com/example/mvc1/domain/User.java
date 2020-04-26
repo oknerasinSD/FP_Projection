45830fce8d3d0232eb4a16fabf99863a8658977c
oknerasinSD <d.oknerasins@gmail.com>
2020-04-26 16:19:29 +0300

    Implement mail sending and account activation

--- src/main/java/com/example/mvc1/domain/User.java
+++ src/main/java/com/example/mvc1/domain/User.java
@@ -20,6 +20,8 @@ public class User implements UserDetails {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+    private String email;
+    private String activationCode;
 
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
@@ -88,5 +90,21 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
